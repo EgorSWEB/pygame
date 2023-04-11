025ff1cf773a0af8d578fa16b78710e641de3b8a
EgorSWEB <43416749+EgorSWEB@users.noreply.github.com>
2023-04-11 20:54:22 +0300

    Add files via upload
    добавили подпрограммы

--- lesson_21_02.py
+++ lesson_21_02.py
@@ -1,57 +1,9 @@
 import pygame
 import random
 
-COLOR = (100, 100, 0)
-W = 1000
-H = 800
-SIZE = (W, H)
-FPS = 60
-WW = 20
-R_C = 15
-
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode(SIZE)
-screen.fill(COLOR)
-pygame.display.set_caption("Моя игра")
-pygame.draw.rect(screen, (0, 0, 0), (0, 0, W, H + WW), WW)
-
-x_circle = W // 2
-y_circle = H // 2 + 300
-vx_circle = 0
-vy_circle = 0
-
-w_plat = 100
-h_plat = 5
-x_plat = W // 2 - w_plat // 2
-y_plat = H // 2 + 300 + R_C + 1
-
-W_B = 80
-H_B = 30
-
-pygame.draw.circle(screen, (255, 255, 255), (x_circle, y_circle), R_C)
-pygame.draw.rect(screen, (0, 0, 0), (x_plat, y_plat, w_plat, h_plat), 0)
-
-blocks = []
-for i in range(5):
-    blocks.append(pygame.draw.rect(screen, (0, 0, 0), ((W_B + 10) * i + 300, 150, W_B, H_B), 0))
-print(blocks)  
-
-
-running = True
-loose = False
-
-while running:
-    pygame.time.wait(1000 // FPS)
-    screen.fill(COLOR)
-    for i in range(len(blocks)):
-        pygame.draw.rect(screen, (0, 0, 0), blocks[i], 0)
+def processing_blocks():
+    global blocks, circle, no_jump, vx_circle, vy_circle, cnt, x_circle, y_circle, W_B, H_B, W, WW
     
-    
-    pygame.draw.rect(screen, (0, 0, 0), (0, 0, W, H + WW), WW)
-    circle = pygame.draw.circle(screen, (255, 255, 255), (x_circle, y_circle), R_C)
-    
-
     no_jump = True
 
     for i in range(len(blocks) - 1, -1, -1):
@@ -99,13 +51,80 @@ while running:
                         vx_circle *= -1
                 no_jump = False
             blocks.pop(i)
-    
-    plat = pygame.draw.rect(screen, (0, 0, 0), (x_plat, y_plat, w_plat, h_plat), 0)
+            cnt += 1
+            blocks.append(pygame.draw.rect(screen, (0, 0, 0), ((random.randint(WW, W - WW - W_B), random.randint(WW, 200), W_B, H_B)), 0))
 
+def reflection_wall():
+    global blocks, circle, no_jump, vx_circle, vy_circle
+    
     if x_circle >= W - WW - R_C or x_circle <= R_C + WW:
         vx_circle = vx_circle * (-1)
     if y_circle <= WW + R_C:
         vy_circle = vy_circle * (-1)
+
+
+COLOR = (100, 100, 0)
+W = 1000
+H = 800
+SIZE = (W, H)
+FPS = 60
+WW = 20
+R_C = 15
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode(SIZE)
+screen.fill(COLOR)
+pygame.display.set_caption("Моя игра")
+pygame.draw.rect(screen, (0, 0, 0), (0, 0, W, H + WW), WW)
+
+x_circle = W // 2
+y_circle = H // 2 + 300
+vx_circle = 0
+vy_circle = 0
+
+w_plat = 100
+h_plat = 5
+x_plat = W // 2 - w_plat // 2
+y_plat = H // 2 + 300 + R_C + 1
+
+W_B = 80
+H_B = 30
+
+pygame.draw.circle(screen, (255, 255, 255), (x_circle, y_circle), R_C)
+pygame.draw.rect(screen, (0, 0, 0), (x_plat, y_plat, w_plat, h_plat), 0)
+
+blocks = []
+for i in range(5):
+    blocks.append(pygame.draw.rect(screen, (0, 0, 0), ((W_B + 10) * i + 300, 150, W_B, H_B), 0))
+print(blocks)  
+
+
+running = True
+loose = False
+cnt = 0
+
+pygame.font.init()
+font_cnt = pygame.font.SysFont('Comic Sans MS', 20)
+data = 'СЧЁТ:'
+ts_2 = font_cnt.render(data + str(cnt), True, (255, 255, 255))
+screen.blit(ts_2, (WW + 10, WW))
+
+while running:
+    pygame.time.wait(1000 // FPS)
+    screen.fill(COLOR)
+    for i in range(len(blocks)):
+        pygame.draw.rect(screen, (0, 0, 0), blocks[i], 0)
+    
+    
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, W, H + WW), WW)
+    circle = pygame.draw.circle(screen, (255, 255, 255), (x_circle, y_circle), R_C)
+    
+    processing_blocks()
+
+    plat = pygame.draw.rect(screen, (0, 0, 0), (x_plat, y_plat, w_plat, h_plat), 0)
+
+    reflection_wall()
 
     if plat.colliderect(circle):
         if(y_circle >= y_plat - R_C and y_circle + R_C <= y_plat + h_plat and x_circle >= x_plat and x_circle <= x_plat + w_plat):
@@ -130,6 +149,8 @@ while running:
             if x_plat > WW + 40:
                 x_plat -= 40
         
+    ts_2 = font_cnt.render(data + str(cnt), True, (255, 255, 255))
+    screen.blit(ts_2, (WW + 10, WW))
     pygame.display.flip()
 
     if (y_circle > H):
@@ -139,7 +160,6 @@ while running:
 running = True
 
 if loose:
-    pygame.font.init() # Инициализация модуля font
     font = pygame.font.SysFont('Comic Sans MS', 100)
 # Шрифт Comic Sans MS, размер 15
     data = 'GAME OVER'

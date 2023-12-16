31e126dd8191bf2cb81062d36d2094a79f0bd78f
EgorSWEB <43416749+EgorSWEB@users.noreply.github.com>
2023-12-16 13:15:01 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,7 +1,41 @@
-file = open('workwithfiles/table.txt', 'a+', encoding="utf-8")
-file.write("Таблетки|2 р/д, 7 дней|06.12.23\n")
-file.seek(0) # переносит каретку для чтения в начало файла
-file.write("Таблетки|2 р/д, 8 дней|06.12.23\n")
-print(file.readline().split('|'))
-print(file.readline().split('|'))
+import datetime
+file = open('table.txt', 'a+', encoding="utf-8")
+while True:
+    com = input()
+
+    if com == 'add':
+        name = input('Название заметки:')
+        body = input('Заметка:')
+        file.write(name + '|' + body + '|' + str(datetime.datetime.now()) + '\n')
+        print('Заметка добавлена!')
+    
+    if com == 'all':
+        print('Все заметки:')
+        file.seek(0)
+        for line in file:
+            arr = line.split('|')
+            print('----------------------------------')
+            print('Название:', arr[0])
+            print('Заметка:', arr[1])
+            print('Дата:', arr[2])
+    
+    if com == 'find':
+        name = input('Название заметки:')
+        found = False
+        file.seek(0)
+        for line in file:
+            if line.find(name + '|') == 0:
+                arr = line.split('|')
+                print('Найдена заметка!')
+                print('Название:', arr[0])
+                print('Заметка:', arr[1])
+                print('Дата:', arr[2])
+                found = True
+                break
+
+        if not found:
+            print('Заметок с таким названием нет :(')
+
+    if com == 'exit':
+        break
 file.close()
